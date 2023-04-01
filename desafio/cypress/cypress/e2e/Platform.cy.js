99fbf50a6c897993c211f926d94d627783820434
wend <wend@pop-os.localdomain>
2023-04-01 11:34:24 -0300

    testes dentro da plataforma

--- desafio/cypress/cypress/e2e/Platform.cy.js
+++ desafio/cypress/cypress/e2e/Platform.cy.js
@@ -10,16 +10,46 @@ describe('Testando dentro da plataforma',()=>{
         cy.contains('a','Logout').should('be.visible');
     })
 
-    it.only('Verificar mensagem de erro na descrição',()=>{
+    //Esse teste é meio triste
+    //Esse aqui tem um problema da tela abrir a janela de selecionar o arquivo e eu não consegui achar como fechar com cypress
+    // it.skip('Verificar mensagem de erro na descrição',()=>{
+    //     cy.get('input[formcontrolname="userName"]').type('weu1');
+    //     cy.get('input[formcontrolname="password"]').type('123456789');
+    //     cy.get('.btn').click();
+    //     cy.get(':nth-child(2) > a > .fa').click();
+    //     cy.wait(1000);
+    //     cy.focused().type('{alt}{f4}')
+    // });
+
+    it('Verificar mensagem de maximo caracteres np comentário',()=>{
         cy.get('input[formcontrolname="userName"]').type('weu1');
         cy.get('input[formcontrolname="password"]').type('123456789');
         cy.get('.btn').click();
-        cy.get(':nth-child(2) > a > .fa').click();
-        cy.get('input[type="file"]').then(subject => {
-            cy.wrap(subject).trigger('click').then(() => {
-                cy.get('body').type('{esc}')
-            })
-        })
-
+        cy.get(':nth-child(1) > a > ap-card > .card > .card-block > ap-photo > .img-thumbnail').click();
+        cy.get('textarea[formcontrolname="comment"]').type("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+        cy.contains('ap-vmessage', 'Maximun size allowed is 300').should('be.visible');
     });
+
+    //Nesse teste é preciso uma imagem upada no site para que ele possa apagar.
+    it('Verificar mensagem de confirmação ao apagar',()=>{
+        cy.get('input[formcontrolname="userName"]').type('weu1');
+        cy.get('input[formcontrolname="password"]').type('123456789');
+        cy.get('.btn').click();
+        cy.get(':nth-child(1) > a > ap-card > .card > .card-block > ap-photo > .img-thumbnail').click();
+        cy.get('.fa-trash-o').click();
+        cy.get('ap-alert > .text-center').should('contain', 'Photo removed');
+    });
+
+    //Como a pesquisa é dinamica só verifico se o input ta recebendo o texto, pois pode existir ou não uma foto.
+    it('Verificar caixa de pesquisa',()=>{
+        cy.get('input[formcontrolname="userName"]').type('weu1');
+        cy.get('input[formcontrolname="password"]').type('123456789');
+        cy.get('.btn').click();
+       cy.get('input[type="search"]').type("teste");
+        cy.get('input[type="search"]').should('have.value', 'teste');
+    });
+
+
+
+
 })
