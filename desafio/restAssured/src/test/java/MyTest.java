3161a73d1239d70eca8bba733fde0873173487d8
wend <wend@pop-os.localdomain>
2023-04-05 23:51:25 -0300

    feature/ runner construído

--- desafio/restAssured/src/test/java/MyTest.java
+++ desafio/restAssured/src/test/java/MyTest.java
@@ -1,9 +1,10 @@
+import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.Test;
 
-@CucumberOptions(features = "src/test/resources/features", glue = "steps")
-public class MyTest {
-    @Test
-    public void runTests(){
-    }
+@CucumberOptions(features = "src/test/resources/features",
+        glue = "steps",
+        plugin = {"pretty","html:target/cucumber-reports/cucumber-pretty"})
+public class MyTest extends AbstractTestNGCucumberTests {
+
 }
